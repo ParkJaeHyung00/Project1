e747c1bb46c142efa534a7f517510c8bd2490675
ParkJaeHyung <psy970825@naver.com>
2024-04-29 15:37:06 +0900

    2번째 커밋 기존의 계산기에 빼기 함수 추가

--- main.py
+++ main.py
@@ -2,10 +2,16 @@
 def add_func(n1,n2):
     retVal=n1+n2
     return retVal
+
+def sub_func(n1,n2):
+    retVal=n1-n2
+    return retVal
+
 ##전역 변수
 num1,num2,res=100,200,0
 
-
-
 res=add_func(num1,num2)
+print(num1,'+',num2,'=',res)
+
+res=sub_func(num1,num2)
 print(num1,'+',num2,'=',res)
